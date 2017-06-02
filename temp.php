34d14d2fad7a4a255d57e92ebad90d34d4d9e136
foreveryounger <andrexaoax@gmail.com>
2017-06-03 00:53:29 +0700

    Create temp.php

---
+++ temp.php
@@ -0,0 +1,24 @@
+<div id="muvipro-posts-2" class="home-widget widget muvipro-posts-module"><div class="row"><div class="col-md-10"><h3 class="homemodule-title">Genre Movie</h3></div><div class="col-md-2"><div class="module-linktitle"><h4><a href="http://example.com/genre/genremovie/" title="Watch Genre Movie">More</a></h4></div></div></div>
+			<div class="row grid-container gmr-module-posts">
+									<div class="col-md-125" itemscope="itemscope" itemtype="http://schema.org/Movie">
+						<div class="gmr-item-modulepost">
+							<a href="http://example.com/movie-name/" itemprop="url" title="Watch Movie Name" rel="bookmark"><img width="152" height="228" src="http://example.com/size-152x228.jpg" class="attachment-medium size-medium wp-post-image" alt="" itemprop="image" srcset="http://example.com/size-152x228.jpg 152w, http://example.com/size-60x90.jpg 60w, http://example.com/size-170x255.jpg 170w, http://example.com/size.jpg 300w" sizes="(max-width: 152px) 100vw, 152px" title="size" style="height: 188px;"></a>							
+							<header class="entry-header text-center">
+								<div class="gmr-button-widget">
+									<div class="clearfix gmr-popup-button-widget"><a href="https://www.youtube.com/watch?v=RllJtOw0USI" class="button gmr-trailer-popup" title="Trailer Movie Name">Trailer</a></div>									<div class="clearfix">
+										<a href="http://example.com/movie-name/" class="button" itemprop="url" title="Watch Movie Name" rel="bookmark">Watch Movie</a>
+									</div>
+								</div>
+								<h2 class="entry-title" itemprop="headline">
+									<a href="http://example.com/movie-name/" itemprop="url" title="Watch Movie Name" rel="bookmark">
+										Movie Name									</a>
+								</h2>
+							</header><!-- .entry-header -->
+							
+							<div class="gmr-rating-item">Rating: 10</div><div class="gmr-duration-item" property="duration">100 minutes</div><div class="gmr-quality-item"><a href="http://example.com/quality/hd/" rel="tag">1080p</a></div><span class="screen-reader-text"><time itemprop="dateCreated" datetime="2017-02-14T00:00:00+00:00">14 Feb 2017</time></span><span class="screen-reader-text"><span itemprop="director" itemscope="itemscope" itemtype="http://schema.org/Person"><span itemprop="name"><a href="http://example.com/director/director-name/" rel="tag">Director Name</a></span></span>, <span itemprop="director" itemscope="itemscope" itemtype="http://schema.org/Person"><span itemprop="name"><a href="http://example.com/director/director-name/" rel="tag">Director</a></span></span></span>								
+						</div>
+					
+					</div>
+							</div>
+			
+		</div>
